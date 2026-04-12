6f6cb24018ac7df23b1674a6699606042545ce75
Fozzyishere <danielpham13092002@gmail.com>
2026-04-12 15:39:11 +0700

    fix; remove hard-coded test suite

--- OsuInstaFadeSkinGenerator.Tests/InstaFadeGeneratorTests.cs
+++ OsuInstaFadeSkinGenerator.Tests/InstaFadeGeneratorTests.cs
@@ -10,8 +10,9 @@ public sealed class InstaFadeGeneratorTests
     public async Task GenerateAsync_Template2_PerformsInstaFadeWorkflowAndUpdatesSkinIni()
     {
         using var skinDir = new TestSkinDirectory();
-        const string prefix = "default";
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template2SkinIni);
+        var templateContent = SkinIniTemplateFixture.GetTemplateContent(2);
+        var prefix = SkinIniTemplateFixture.ParseSupportedFields(templateContent).HitCirclePrefix;
+        SkinIniTemplateFixture.WriteTemplateSkinIni(skinDir.RootPath, 2);
         CreateBaseAssets(skinDir.RootPath);
         SkinTestHelper.CreateNumberAssets(skinDir.RootPath, prefix);
 
@@ -49,23 +50,16 @@ public sealed class InstaFadeGeneratorTests
         SkinTestHelper.AssertFullyTransparent(Path.Combine(skinDir.RootPath, "hitcircleoverlay.png"));
 
         var updatedSkinIni = File.ReadAllText(Path.Combine(skinDir.RootPath, "skin.ini"));
-        Assert.Contains("Combo1: 10,20,30", updatedSkinIni);
-        Assert.DoesNotContain("Combo2:", updatedSkinIni);
-        Assert.DoesNotContain("Combo3:", updatedSkinIni);
-        Assert.DoesNotContain("Combo4:", updatedSkinIni);
-        Assert.Contains("HitCircleOverlap: 5", updatedSkinIni);
-        Assert.Contains("HitCirclePrefix: default", updatedSkinIni);
-        Assert.Contains("ScorePrefix: score", updatedSkinIni);
-        Assert.Contains("MenuGlow: 145,191,255 // #91bfff", updatedSkinIni);
-        Assert.Contains("MenuGlow: 17,157,244 // #119df4", updatedSkinIni);
+        SkinIniTemplateFixture.AssertUpdatedSkinIni(templateContent, updatedSkinIni, "10,20,30", 5);
     }
 
     [Fact]
     public async Task GenerateAsync_Template4_BacksUpOriginalAssetsGeneratedNumbersAndSkinIni()
     {
         using var skinDir = new TestSkinDirectory();
-        const string prefix = "default";
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template4SkinIni);
+        var templateContent = SkinIniTemplateFixture.GetTemplateContent(4);
+        var prefix = SkinIniTemplateFixture.ParseSupportedFields(templateContent).HitCirclePrefix;
+        SkinIniTemplateFixture.WriteTemplateSkinIni(skinDir.RootPath, 4);
         CreateBaseAssets(skinDir.RootPath);
         SkinTestHelper.WriteFilledPng(Path.Combine(skinDir.RootPath, "hitcircle@2x.png"), 8, 8, new Rgba32(200, 0, 0, 255));
         SkinTestHelper.WriteFilledPng(Path.Combine(skinDir.RootPath, "hitcircleoverlay@2x.png"), 8, 8, new Rgba32(0, 200, 0, 255));
@@ -84,21 +78,21 @@ public sealed class InstaFadeGeneratorTests
         Assert.True(File.Exists(Path.Combine(backupDir, "hitcircleoverlay.png")));
         Assert.True(File.Exists(Path.Combine(backupDir, "hitcircle@2x.png")));
         Assert.True(File.Exists(Path.Combine(backupDir, "hitcircleoverlay@2x.png")));
-        Assert.True(File.Exists(Path.Combine(backupDir, "default-1.png")));
-        Assert.True(File.Exists(Path.Combine(backupDir, "default-5@2x.png")));
+        Assert.True(File.Exists(SkinTestHelper.ResolvePrefixPath(backupDir, prefix, "1")));
+        Assert.True(File.Exists(SkinTestHelper.ResolvePrefixPath(backupDir, prefix, "5", "@2x")));
         Assert.True(File.Exists(Path.Combine(backupDir, "skin.ini")));
 
         var backedUpSkinIni = File.ReadAllText(Path.Combine(backupDir, "skin.ini"));
-        Assert.Contains("Combo2: 206,137,137 // #ce8989", backedUpSkinIni);
-        Assert.Contains("HitCircleOverlayAboveNumber: 0", backedUpSkinIni);
+        Assert.Equal(templateContent, backedUpSkinIni);
     }
 
     [Fact]
     public async Task GenerateAsync_Template1_SkipsHdWhenRequestedWithoutRequiredHdAssets()
     {
         using var skinDir = new TestSkinDirectory();
-        const string prefix = "default";
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template1SkinIni);
+        var templateContent = SkinIniTemplateFixture.GetTemplateContent(1);
+        var prefix = SkinIniTemplateFixture.ParseSupportedFields(templateContent).HitCirclePrefix;
+        SkinIniTemplateFixture.WriteTemplateSkinIni(skinDir.RootPath, 1);
         CreateBaseAssets(skinDir.RootPath);
         SkinTestHelper.CreateNumberAssets(skinDir.RootPath, prefix);
         var progress = new RecordingProgress();
@@ -114,17 +108,16 @@ public sealed class InstaFadeGeneratorTests
         Assert.False(File.Exists(SkinTestHelper.ResolvePrefixPath(skinDir.RootPath, prefix, "1", "@2x")));
 
         var updatedSkinIni = File.ReadAllText(Path.Combine(skinDir.RootPath, "skin.ini"));
-        Assert.Contains("Combo1: 10,20,30", updatedSkinIni);
-        Assert.DoesNotContain("Combo2:", updatedSkinIni);
-        Assert.Contains("HitCircleOverlap: 5", updatedSkinIni);
+        SkinIniTemplateFixture.AssertUpdatedSkinIni(templateContent, updatedSkinIni, "10,20,30", 5);
     }
 
     [Fact]
     public async Task GenerateAsync_Template3_TripleStackingRestoresMergedBaseAssetsAndKeepsMania()
     {
         using var skinDir = new TestSkinDirectory();
-        const string prefix = "default";
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template3SkinIni);
+        var templateContent = SkinIniTemplateFixture.GetTemplateContent(3);
+        var prefix = SkinIniTemplateFixture.ParseSupportedFields(templateContent).HitCirclePrefix;
+        SkinIniTemplateFixture.WriteTemplateSkinIni(skinDir.RootPath, 3);
         CreateBaseAssets(skinDir.RootPath);
         SkinTestHelper.CreateNumberAssets(skinDir.RootPath, prefix);
         var generator = CreateGenerator();
@@ -147,12 +140,7 @@ public sealed class InstaFadeGeneratorTests
         Assert.Equal(new Rgba32(0, 255, 0, 255), hitcircle[2, 2]);
 
         var updatedSkinIni = File.ReadAllText(Path.Combine(skinDir.RootPath, "skin.ini"));
-        Assert.Equal(2, updatedSkinIni.Split("[Mania]", StringSplitOptions.None).Length - 1);
-        Assert.Contains("Keys: 1", updatedSkinIni);
-        Assert.Contains("Keys: 2", updatedSkinIni);
-        Assert.Contains("Combo1: 10,20,30", updatedSkinIni);
-        Assert.DoesNotContain("Combo2:", updatedSkinIni);
-        Assert.DoesNotContain("Combo3:", updatedSkinIni);
+        SkinIniTemplateFixture.AssertUpdatedSkinIni(templateContent, updatedSkinIni, "10,20,30", 5);
     }
 
     private static InstaFadeGenerator CreateGenerator()
@@ -201,346 +189,4 @@ public sealed class InstaFadeGeneratorTests
             this.Entries.Add(value);
         }
     }
-
-    private const string Template1SkinIni =
-        """
-        //Formatted by ck // pepega tools // cyperdark#6890 // https://github.com/cyperdark/ck-tools
-        [General]
-            Name: -         《CK》 WhiteCat 2.1 ~ new
-            Author: cyperdark
-            Profile: https://osu.ppy.sh/users/9893708
-            ||=====
-            || Downloaded from https://ck1t.ru/ss
-            || Skin https://ck1t.ru/s-WhiteCat 2.1 (CK)
-            ||=====
-
-            Version: 2.5
-            AnimationFramerate: 60
-
-        // -=- Slider 
-            AllowSliderBallTint: 1
-
-        // -=- Combo bursts 
-            ComboBurstRandom: 0
-            SliderBallFlip: 1
-
-        // -=- Cursor 
-            CursorExpand: 0
-            CursorCentre: 1
-            CursorRotate: 0
-            CursorTrailRotate: 0
-
-        [Colours]
-        // -=- Combo Colors 
-            Combo1: 206, 188, 178 // #CEBCB2
-            Combo2: 237, 221, 213 // #EDDDD5
-
-        // -=- Misc Colors 
-            InputOverlayText: 254, 237, 227 // #FEEDE3
-
-        // -=- Song Select 
-            SongSelectActiveText: 255, 255, 255 // #FFFFFF
-            SongSelectInactiveText: 200, 200, 200 // #C8C8C8
-            MenuGlow: 205, 186, 177 // #CDBAB1
-
-        // -=- Colors 
-            SpinnerBackground: 255, 255, 255 // #FFFFFF
-
-        // -=- Slider Colors 
-            SliderBorder: 80, 80, 80 // #505050
-            SliderTrackOverride: 0, 0, 0 // #000000
-
-        [Fonts]
-        // -=- HitCircle 
-            HitCircleOverlap: 15
-
-        // -=- Score 
-            ScorePrefix: numbers
-            ScoreOverlap: 0
-            ComboPrefix: numbers
-            ComboOverlap: 0
-        """;
-
-    private const string Template2SkinIni =
-        """
-        //Formatted by ck // pepega tools // cyperdark#6890
-        [General]
-            Name: - JesusOmega {NM} 『Planets』 -
-            Author: JesusOmega
-            Profile: https://osu.ppy.sh/users/11080396
-            ╔=====================================╗
-            ║ Downloaded from https://ck1t.ru/ss  ╚================╗
-            ║ Skin https://ck1t.ru/s-- JesusOmega {NM} 『Planets』 - ║
-            ╚======================================================╝
-            Version: latest
-            AnimationFramerate: 46
-
-        // ╔════ Cursor ════╗ \\
-            CursorCentre: 1
-            CursorExpand: 1
-            CursorRotate: 0
-
-        // ╔════ Combo bursts ════╗ \\
-            ComboBurstRandom: 0
-
-        // ╔════ Slider ════╗ \\
-            AllowSliderBallTint: 1
-            SliderBallFlip: 0
-            SliderBallFrames: 1
-            SliderStyle: 2
-
-        // ╔════ Spinner ════╗ \\
-            SpinnerNoBlink: 1
-
-        [Colours]
-        // ╔════ Combo Colors ════╗ \\
-            Combo1: 255, 105, 125 // #ff697d
-            Combo2: 224, 177, 252 // #e0b1fc
-            Combo3: 131, 180, 252 // #83b4fc
-            Combo4: 88, 196, 112 // #58c470
-            MenuGlow: 145,191,255 // #91bfff
-            MenuGlow: 17,157,244 // #119df4
-            InputOverlayText: 255,255,255 // #ffffff
-            SliderBorder: 205,192,236 // #cdc0ec
-            SliderTrackOverride: 10,10,10 // #0a0a0a
-
-        // ╔════ Song Select ════╗ \\
-            SongSelectActiveText: 130,203,255 // #82cbff
-            SongSelectInactiveText: 120,120,120 // #787878
-
-        [Fonts]
-        // ╔════ HitCircle ════╗ \\
-            HitCirclePrefix: default
-            HitCircleOverlap: 26
-
-        // ╔════ Score ════╗ \\
-            ScorePrefix: score
-            ScoreOverlap: 8
-
-        // ╔════ Combo ════╗ \\
-            ComboPrefix: score
-        """;
-
-    private const string Template3SkinIni =
-        """
-        // Ini parser v0.2.9 // cyperdark#6890 // https://github.com/cyperdark/ck-tools
-
-
-        [General]
-            Name: BubbleSkin-EditCoquis v2
-            Author: Various
-            Version: 2.0
-
-        // -=- Cursor
-            CursorCentre: 1
-            CursorExpand: 0
-            CursorRotate: 0
-            CursorTrailRotate: 0
-
-        // -=- Slider
-            AllowSliderBallTint: 1
-            SliderBallFlip: 1
-
-        // -=- Spinner
-            SpinnerFadePlayfield: 1
-
-
-        [Colours]
-        // -=- Combo Colors
-            Combo1: 128,131,253
-            Combo2: 130,253,207
-            Combo3: 15,177,255
-
-        // -=- Misc Colors
-            InputOverlayText: 255,255,255
-
-        // -=- Slider Colors
-            SliderBorder: 70,70,70
-            SliderTrackOverride: 0,0,20
-
-        // -=- Song Select
-            SongSelectActiveText: 250,250,250
-            SongSelectInactiveText: 230,230,230
-
-
-        [Fonts]
-        // -=- Circle Font
-            HitCirclePrefix: default
-            HitCircleOverlap: 10
-
-        // -=- Score Font
-            ScorePrefix: score
-            ScoreOverlap: 7
-
-        // -=- Combo Font
-            ComboPrefix: combo
-            ComboOverlap: 3
-
-
-        [Mania]
-            Keys: 1
-            WarningArrow: arrow-scroll
-            BarlineHeight: 0
-            LightFramePerSecond: 60
-
-        // -=- Toggles
-            KeysUnderNotes: 1
-            JudgementLine: 0
-            UpsideDown: 0
-            SpecialStyle: 0
-
-        // -=- Position
-            HitPosition: 458
-            ScorePosition: 280
-            ComboPosition: 160
-
-        // -=- Column
-            ColumnStart: 393
-            ColumnLineWidth: 0,0
-            ColumnWidth: 68
-
-        // -=- Lighting
-            LightingNWidth: 62
-            LightingLWidth: 62
-            LightingN: lightingA
-            LightingL: lightingA
-
-        // -=- Stage
-            StageHint: blank-hint
-
-        // -=- Note Style
-            NoteBodyStyle: 0
-
-        // -=- Colors
-            Colour1: 0,0,0,0
-            ColourBarline: 250,175,255,150
-            ColourColumnLine: 30,30,30,30
-            ColourHold: 255,255,255,255
-
-        // -=- Key Image
-            KeyImage0: Orbs\k_down
-            KeyImage0D: Orbs\k_down
-
-        // -=- Note Image
-            NoteImage0: Orbs\White
-            NoteImage0H: Orbs\White
-            NoteImage0L: Orbs\Holdbodyw
-            NoteImage0T: Orbs\Holdcapw
-
-
-        [Mania]
-            Keys: 2
-            WarningArrow: arrow-scroll
-            BarlineHeight: 0
-            LightFramePerSecond: 60
-
-        // -=- Toggles
-            KeysUnderNotes: 1
-            JudgementLine: 0
-            UpsideDown: 0
-            SpecialStyle: 0
-
-        // -=- Position
-            HitPosition: 458
-            ScorePosition: 280
-            ComboPosition: 160
-
-        // -=- Column
-            ColumnStart: 356
-            ColumnLineWidth: 0,0,0
-            ColumnWidth: 68,68
-            ColumnSpacing: 5
-
-        // -=- Lighting
-            LightingNWidth: 62,62
-            LightingLWidth: 62,62
-            LightingN: lightingA
-            LightingL: lightingA
-
-        // -=- Stage
-            StageHint: blank-hint
-
-        // -=- Note Style
-            NoteBodyStyle: 0
-
-        // -=- Colors
-            Colour1: 0,0,0,0
-            Colour2: 0,0,0,0
-            ColourBarline: 250,175,255,150
-            ColourColumnLine: 30,30,30,30
-            ColourHold: 255,255,255,255
-
-        // -=- Key Image
-            KeyImage0: Orbs\k_down
-            KeyImage0D: Orbs\k_down
-            KeyImage1: Orbs\k_down
-            KeyImage1D: Orbs\k_down
-
-        // -=- Note Image
-            NoteImage0: Orbs\White
-            NoteImage0H: Orbs\White
-            NoteImage0L: Orbs\Holdbodyw
-            NoteImage1: Orbs\Blue
-            NoteImage1H: Orbs\Blue
-            NoteImage1L: Orbs\Holdbodyb
-            NoteImage0T: Orbs\Holdcapw
-            NoteImage1T: Orbs\Holdcapb
-        """;
-
-    private const string Template4SkinIni =
-        """
-        //Formatted by ck // pepega tools // cyperdark#6890
-        [General]
-            Name: -         《CK》 Bacon boi 1.0
-            Author: cyperdark
-            Profile: https://osu.ppy.sh/users/9893708
-            ╔=====================================╗
-            ║ Downloaded from https://ck1t.ru/ss  ╚===============╗
-            ║ Skin https://ck1t.ru/s--         《CK》 Bacon boi 1.0 ║
-            ╚=====================================================╝
-            Version: 2.5
-            AnimationFramerate: 60
-
-        // ╔════ Cursor ════╗ \\
-            CursorCentre: 1
-            CursorExpand: 0
-            CursorRotate: 0
-            CursorTrailRotate: 0
-
-        // ╔════ Combo bursts ════╗ \\
-            ComboBurstRandom: 0
-            HitCircleOverlayAboveNumber: 0
-
-        // ╔════ Slider ════╗ \\
-            AllowSliderBallTint: 1
-            SliderBallFlip: 1
-            SliderStyle: 2
-
-        [Colours]
-        // ╔════ Combo Colors ════╗ \\
-            Combo1: 241,214,207 // #f1d6cf
-            Combo2: 206,137,137 // #ce8989
-            MenuGlow: 82, 74, 71 // #524a47
-            InputOverlayText: 255, 255, 255 // #ffffff
-            SliderBorder: 113, 102, 98 // #716662
-            SliderTrackOverride: 20, 18, 17 // #141211
-
-        // ╔════ Song Select ════╗ \\
-            SongSelectActiveText: 255, 255, 255 // #ffffff
-            SongSelectInactiveText: 255, 255, 255 // #ffffff
-            SpinnerBackground: 255, 255, 255 // #ffffff
-
-        [Fonts]
-        // ╔════ HitCircle ════╗ \\
-            HitCirclePrefix: default
-            HitCircleOverlap: 25
-
-        // ╔════ Score ════╗ \\
-            ScorePrefix: numbers
-            ScoreOverlap: 10
-
-        // ╔════ Combo ════╗ \\
-            ComboPrefix: numbers
-            ComboOverlap: 10
-        """;
 }
--- OsuInstaFadeSkinGenerator.Tests/SkinIniWriterTests.cs
+++ OsuInstaFadeSkinGenerator.Tests/SkinIniWriterTests.cs
@@ -4,61 +4,31 @@ namespace OsuInstaFadeSkinGenerator.Tests;
 
 public sealed class SkinIniWriterTests
 {
-    [Fact]
-    public void Update_Template2_RewritesToSingleComboAndPreservesUnrelatedLines()
+    [Theory]
+    [InlineData(2, 7, 8, 9, 64)]
+    [InlineData(3, 4, 5, 6, 77)]
+    public void Update_Template_RewritesSupportedFieldsAndPreservesOtherLines(
+        int templateNumber,
+        byte comboR,
+        byte comboG,
+        byte comboB,
+        int hitCircleOverlap)
     {
         using var skinDir = new TestSkinDirectory();
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template2SkinIni);
+        var originalContent = SkinIniTemplateFixture.GetTemplateContent(templateNumber);
+        SkinIniTemplateFixture.WriteTemplateSkinIni(skinDir.RootPath, templateNumber);
 
         var writer = new SkinIniWriter();
         var skinIniPath = Path.Combine(skinDir.RootPath, "skin.ini");
 
-        writer.Update(skinIniPath, 7, 8, 9, 64);
+        writer.Update(skinIniPath, comboR, comboG, comboB, hitCircleOverlap);
 
         var updated = File.ReadAllText(skinIniPath);
-
-        Assert.Contains("Name: - JesusOmega {NM} 『Planets』 -", updated);
-        Assert.Contains("Profile: https://osu.ppy.sh/users/11080396", updated);
-        Assert.Contains("    Combo1: 7,8,9", updated);
-        Assert.DoesNotContain("Combo2:", updated);
-        Assert.DoesNotContain("Combo3:", updated);
-        Assert.DoesNotContain("Combo4:", updated);
-        Assert.Contains("    MenuGlow: 145,191,255 // #91bfff", updated);
-        Assert.Contains("    MenuGlow: 17,157,244 // #119df4", updated);
-        Assert.Contains("    SliderBorder: 205,192,236 // #cdc0ec", updated);
-        Assert.Contains("    SliderTrackOverride: 10,10,10 // #0a0a0a", updated);
-        Assert.Contains("    HitCirclePrefix: default", updated);
-        Assert.Contains("    HitCircleOverlap: 64", updated);
-        Assert.Contains("    ScorePrefix: score", updated);
-        Assert.Contains("    ComboPrefix: score", updated);
-    }
-
-    [Fact]
-    public void Update_Template3_PreservesMultipleManiaSectionsAndNoisyFormatting()
-    {
-        using var skinDir = new TestSkinDirectory();
-        SkinTestHelper.WriteSkinIni(skinDir.RootPath, Template3SkinIni);
-
-        var writer = new SkinIniWriter();
-        var skinIniPath = Path.Combine(skinDir.RootPath, "skin.ini");
-
-        writer.Update(skinIniPath, 4, 5, 6, 77);
-
-        var updated = File.ReadAllText(skinIniPath);
-
-        Assert.Equal(2, updated.Split("[Mania]", StringSplitOptions.None).Length - 1);
-        Assert.Contains("    Combo1: 4,5,6", updated);
-        Assert.DoesNotContain("Combo2:", updated);
-        Assert.DoesNotContain("Combo3:", updated);
-        Assert.Contains("    HitCirclePrefix: default", updated);
-        Assert.Contains("    HitCircleOverlap: 77", updated);
-        Assert.Contains("    ScorePrefix: score", updated);
-        Assert.Contains("    ComboPrefix: combo", updated);
-        Assert.Contains("    Keys: 1", updated);
-        Assert.Contains("    Keys: 2", updated);
-        Assert.Contains("    ColumnSpacing: 5", updated);
-        Assert.Contains(@"    NoteImage0: Orbs\White", updated);
-        Assert.Contains(@"    NoteImage1T: Orbs\Holdcapb", updated);
+        SkinIniTemplateFixture.AssertUpdatedSkinIni(
+            originalContent,
+            updated,
+            $"{comboR},{comboG},{comboB}",
+            hitCircleOverlap);
     }
 
     [Fact]
@@ -93,230 +63,4 @@ public sealed class SkinIniWriterTests
         Assert.Contains("    Author: cyperdark", updated);
         Assert.Contains("    || Downloaded from https://ck1t.ru/ss", updated);
     }
-
-    private const string Template2SkinIni =
-        """
-        //Formatted by ck // pepega tools // cyperdark#6890
-        [General]
-            Name: - JesusOmega {NM} 『Planets』 -
-            Author: JesusOmega
-            Profile: https://osu.ppy.sh/users/11080396
-            ╔=====================================╗
-            ║ Downloaded from https://ck1t.ru/ss  ╚================╗
-            ║ Skin https://ck1t.ru/s-- JesusOmega {NM} 『Planets』 - ║
-            ╚======================================================╝
-            Version: latest
-            AnimationFramerate: 46
-
-        // ╔════ Cursor ════╗ \\
-            CursorCentre: 1
-            CursorExpand: 1
-            CursorRotate: 0
-
-        // ╔════ Combo bursts ════╗ \\
-            ComboBurstRandom: 0
-
-        // ╔════ Slider ════╗ \\
-            AllowSliderBallTint: 1
-            SliderBallFlip: 0
-            SliderBallFrames: 1
-            SliderStyle: 2
-
-        // ╔════ Spinner ════╗ \\
-            SpinnerNoBlink: 1
-
-        [Colours]
-        // ╔════ Combo Colors ════╗ \\
-            Combo1: 255, 105, 125 // #ff697d
-            Combo2: 224, 177, 252 // #e0b1fc
-            Combo3: 131, 180, 252 // #83b4fc
-            Combo4: 88, 196, 112 // #58c470
-            MenuGlow: 145,191,255 // #91bfff
-            MenuGlow: 17,157,244 // #119df4
-            InputOverlayText: 255,255,255 // #ffffff
-            SliderBorder: 205,192,236 // #cdc0ec
-            SliderTrackOverride: 10,10,10 // #0a0a0a
-
-        // ╔════ Song Select ════╗ \\
-            SongSelectActiveText: 130,203,255 // #82cbff
-            SongSelectInactiveText: 120,120,120 // #787878
-
-        [Fonts]
-        // ╔════ HitCircle ════╗ \\
-            HitCirclePrefix: default
-            HitCircleOverlap: 26
-
-        // ╔════ Score ════╗ \\
-            ScorePrefix: score
-            ScoreOverlap: 8
-
-        // ╔════ Combo ════╗ \\
-            ComboPrefix: score
-        """;
-
-    private const string Template3SkinIni =
-        """
-        // Ini parser v0.2.9 // cyperdark#6890 // https://github.com/cyperdark/ck-tools
-
-
-        [General]
-            Name: BubbleSkin-EditCoquis v2
-            Author: Various
-            Version: 2.0
-
-        // -=- Cursor
-            CursorCentre: 1
-            CursorExpand: 0
-            CursorRotate: 0
-            CursorTrailRotate: 0
-
-        // -=- Slider
-            AllowSliderBallTint: 1
-            SliderBallFlip: 1
-
-        // -=- Spinner
-            SpinnerFadePlayfield: 1
-
-
-        [Colours]
-        // -=- Combo Colors
-            Combo1: 128,131,253
-            Combo2: 130,253,207
-            Combo3: 15,177,255
-
-        // -=- Misc Colors
-            InputOverlayText: 255,255,255
-
-        // -=- Slider Colors
-            SliderBorder: 70,70,70
-            SliderTrackOverride: 0,0,20
-
-        // -=- Song Select
-            SongSelectActiveText: 250,250,250
-            SongSelectInactiveText: 230,230,230
-
-
-        [Fonts]
-        // -=- Circle Font
-            HitCirclePrefix: default
-            HitCircleOverlap: 10
-
-        // -=- Score Font
-            ScorePrefix: score
-            ScoreOverlap: 7
-
-        // -=- Combo Font
-            ComboPrefix: combo
-            ComboOverlap: 3
-
-
-        [Mania]
-            Keys: 1
-            WarningArrow: arrow-scroll
-            BarlineHeight: 0
-            LightFramePerSecond: 60
-
-        // -=- Toggles
-            KeysUnderNotes: 1
-            JudgementLine: 0
-            UpsideDown: 0
-            SpecialStyle: 0
-
-        // -=- Position
-            HitPosition: 458
-            ScorePosition: 280
-            ComboPosition: 160
-
-        // -=- Column
-            ColumnStart: 393
-            ColumnLineWidth: 0,0
-            ColumnWidth: 68
-
-        // -=- Lighting
-            LightingNWidth: 62
-            LightingLWidth: 62
-            LightingN: lightingA
-            LightingL: lightingA
-
-        // -=- Stage
-            StageHint: blank-hint
-
-        // -=- Note Style
-            NoteBodyStyle: 0
-
-        // -=- Colors
-            Colour1: 0,0,0,0
-            ColourBarline: 250,175,255,150
-            ColourColumnLine: 30,30,30,30
-            ColourHold: 255,255,255,255
-
-        // -=- Key Image
-            KeyImage0: Orbs\k_down
-            KeyImage0D: Orbs\k_down
-
-        // -=- Note Image
-            NoteImage0: Orbs\White
-            NoteImage0H: Orbs\White
-            NoteImage0L: Orbs\Holdbodyw
-            NoteImage0T: Orbs\Holdcapw
-
-
-        [Mania]
-            Keys: 2
-            WarningArrow: arrow-scroll
-            BarlineHeight: 0
-            LightFramePerSecond: 60
-
-        // -=- Toggles
-            KeysUnderNotes: 1
-            JudgementLine: 0
-            UpsideDown: 0
-            SpecialStyle: 0
-
-        // -=- Position
-            HitPosition: 458
-            ScorePosition: 280
-            ComboPosition: 160
-
-        // -=- Column
-            ColumnStart: 356
-            ColumnLineWidth: 0,0,0
-            ColumnWidth: 68,68
-            ColumnSpacing: 5
-
-        // -=- Lighting
-            LightingNWidth: 62,62
-            LightingLWidth: 62,62
-            LightingN: lightingA
-            LightingL: lightingA
-
-        // -=- Stage
-            StageHint: blank-hint
-
-        // -=- Note Style
-            NoteBodyStyle: 0
-
-        // -=- Colors
-            Colour1: 0,0,0,0
-            Colour2: 0,0,0,0
-            ColourBarline: 250,175,255,150
-            ColourColumnLine: 30,30,30,30
-            ColourHold: 255,255,255,255
-
-        // -=- Key Image
-            KeyImage0: Orbs\k_down
-            KeyImage0D: Orbs\k_down
-            KeyImage1: Orbs\k_down
-            KeyImage1D: Orbs\k_down
-
-        // -=- Note Image
-            NoteImage0: Orbs\White
-            NoteImage0H: Orbs\White
-            NoteImage0L: Orbs\Holdbodyw
-            NoteImage1: Orbs\Blue
-            NoteImage1H: Orbs\Blue
-            NoteImage1L: Orbs\Holdbodyb
-            NoteImage0T: Orbs\Holdcapw
-            NoteImage1T: Orbs\Holdcapb
-        """;
 }
